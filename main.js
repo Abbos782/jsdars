f7062bb6e280f656af59a9d8e82f5f5171a31cce
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-04-07 10:37:20 +0500

    add api

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-document.addEventListener('click', myfunction);
-function myfunction(){
-    console.log("salom dunyo")
+
+
+getData("London")
+
+
+async function getData (shahar){
+    let response = await fetch(`http://api.aladhan.com/v1/timingsByAddress?address=${shahar}&school=1`)
+    let encoding = await response.json()
+    renderData(encoding)
+}   
+
+let tbody = document.querySelector('.tbody')
+
+let obj = {
+    x: 1
+}
+
+// obj.x
+
+
+function renderData(malumot){
+    tbody.textContent = ""
+    let timings = malumot.data.timings
+    for(let item in timings){
+        let newTrElement = document.createElement('tr')
+        let newTimeNameElement = document.createElement('td')
+        let newTimeValueElement = document.createElement('td')
+
+        newTimeNameElement.textContent = item
+        newTimeValueElement.textContent = timings[item]
+
+        newTrElement.appendChild(newTimeNameElement)
+        newTrElement.appendChild(newTimeValueElement)
+        
+        tbody.appendChild(newTrElement)
+    }
 }
